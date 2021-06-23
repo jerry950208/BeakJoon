d7194c749f253213761b874057045adb4fdc1b5e
jerry950208 <jerry950208@naver.com>
2021-06-23 15:54:39 +0900

    BaekJoon

--- 15.동적계획법1/01.피보나치함수.c
+++ 15.동적계획법1/01.피보나치함수.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-int fivo(int n){
-	if(n ==0){
-		printf("0\n");
-		return 0;
-	}
-	else if(n==1){
-		printf("1\n");
-		return 1;
-	}
-	else
-		return fibo(n-1) + fibo(n-2);
-}
+int dp[41][2];
 
 int main(){
-	int n;
-	scanf("%d",&n);
+        int T,N;
+        dp[0][0]=1;
+        dp[1][1]=1;
+        scanf("%d",&T);
+        for(int i=0; i<T; i++){
+                scanf("%d",&N);
+                if(dp[N][0]==0 && dp[N][1]==0)
+                        for(int j=2; j<=N; j++){
+                                dp[j][0]= dp[j-1][0] + dp[j-2][0];
+                                dp[j][1]= dp[j-1][1] + dp[j-2][1];
+                        }
+                printf("%d %d\n",dp[N][0],dp[N][1]);
+        }
 
-	fibo(n);
-	return 0;
+        return 0;
 }
