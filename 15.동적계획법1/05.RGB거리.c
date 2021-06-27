2eeeda116bc723bd874e8a8b1a5308c7e9a925d3
jerry950208 <jerry950208@naver.com>
2021-06-27 11:39:31 +0900

    BaekJoon

--- 15.동적계획법1/05.RGB거리.c
+++ 15.동적계획법1/05.RGB거리.c
@@ -8,7 +8,6 @@ int main(){
 	scanf("%d",&N);
 
 	int rgb[N][3];
-//	int dp[N][3];
 	for(int i=0; i<N; i++){
 		scanf("%d %d %d",&rgb[i][0], &rgb[i][1], &rgb[i][2]);
 		if(i==0){
@@ -17,30 +16,16 @@ int main(){
 			}
 		}
 		else if(i>0){
-			if(rgb[i][0] < rgb[i][1] && rgb[i][0] < rgb[i][2]){
-				dp[i][0] = rgb[i][0] + dp[i-1][1] < dp[i-1][2] ? dp[i-1][1] : dp[i-1][2];
-				printf("R - %d\n",dp[i][0]);
-			}
-			else if(rgb[i][1] < rgb[i][0] && rgb[i][1] < rgb[i][2]){
-				dp[i][1] = rgb[i][1] + dp[i-1][0] < dp[i-1][2] ? dp[i-1][0] : dp[i-1][2];
-				printf("G - %d\n",dp[i][1]);
-			}
-			else if(rgb[i][2] < rgb[i][1] && rgb[i][2] < rgb[i][0]){
-				dp[i][2] = rgb[i][2] + dp[i-1][0] < dp[i-1][1] ? dp[i-1][0] : dp[i-1][1];
-				printf("B - %d\n",dp[i][2]);
-			}
+				dp[i][0] = rgb[i][0] + (dp[i-1][1] < dp[i-1][2] ? dp[i-1][1] : dp[i-1][2]);
+				dp[i][1] = rgb[i][1] + (dp[i-1][0] < dp[i-1][2] ? dp[i-1][0] : dp[i-1][2]);
+				dp[i][2] = rgb[i][2] + (dp[i-1][0] < dp[i-1][1] ? dp[i-1][0] : dp[i-1][1]);
 		}
 	}
-/*
+	
+	N-=1;
 	if(dp[N][0]<dp[N][1])
 		printf("%d\n", dp[N][0] < dp[N][2] ? dp[N][0] : dp[N][2]);	
-	else if(dp[N][0]>dp[N][1])
-		printf("%d\n", dp[N][1] < dp[N][2] ? dp[N][1] : dp[N][2]);
-*/	
-	for(int i=0; i<N; i++){
-		printf("%d %d %d",dp[i][0],dp[i][1],dp[i][2]);
-		printf("\n");
-	}
-		
+	else
+		printf("%d\n", dp[N][1] < dp[N][2] ? dp[N][1] : dp[N][2]);	
 	return 0;
 }
