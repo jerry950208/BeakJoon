fb8f354afc2541f886b64f944ff65c112f44ef5e
jerry950208 <jerry950208@naver.com>
2021-07-01 17:36:21 +0900

    BaekJoon

--- 15.동적계획법1/05.RGB거리.c
+++ 15.동적계획법1/05.RGB거리.c
@@ -21,7 +21,6 @@ int main(){
 				dp[i][2] = rgb[i][2] + (dp[i-1][0] < dp[i-1][1] ? dp[i-1][0] : dp[i-1][1]);
 		}
 	}
-	
 	N-=1;
 	if(dp[N][0]<dp[N][1])
 		printf("%d\n", dp[N][0] < dp[N][2] ? dp[N][0] : dp[N][2]);	
