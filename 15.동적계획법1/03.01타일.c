a77f48234a51b52cad176b7619d17af0d05664b6
jerry950208 <jerry950208@naver.com>
2021-07-01 16:05:24 +0900

    BaekJoon

--- 15.동적계획법1/03.01타일.c
+++ 15.동적계획법1/03.01타일.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 
-long long arr[2]={1,2};
-long long  tile(int n){
-	long long temp;
-	if(n==1){
-		return arr[1];
-	}
-	else{
-		temp=(arr[0] + arr[1]) % 15746;
-		arr[0] = arr[1];
-		arr[1] = temp;
-		return tile(n-1);
-	}
-}
+long long dp[1000000];
 
 int main(){
-	int N;
-	scanf("%d",&N);
-	
-	if(N==1)
-		printf("1\n");
-	else if(N==2)
-		printf("2\n");
-	else 
-		printf("%lld\n",tile(N-1)%15746);
-	
-	return 0;
+        int N;
+        dp[0]=1;
+        dp[1]=2;
+        scanf("%d",&N);
+        for(int i=2; i<N; i++){
+                dp[i]=(dp[i-1] + dp[i-2])%15746;
+        }
+
+        printf("%lld\n",dp[N-1]);
+        return 0;
 }
