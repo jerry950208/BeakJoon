475267da7f2dfbd90971ab00fb1659f95146ade2
jerry950208 <jerry950208@naver.com>
2021-07-01 21:18:45 +0900

    BaekJoon

--- 15.동적계획법1/07.계단오르기.c
+++ 15.동적계획법1/07.계단오르기.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
-int big(int a, int b){
-	return a > b ? a : b;
+int max(int a, int b){
+        return a > b ? a : b;
 }
-
 int main(){
-	int n;
-	scanf("%d",&n);
-	int arr[n+1];
-	int dp[n+1];
+        int N;
+        scanf("%d",&N);
+        int arr[N+1];
+        int dp[N+1];
 
-	for(int i=0; i<n; i++){
-		scanf("%d",&arr[i]);
-	}
-	dp[0] = arr[0];
-	dp[1] = big(arr[0], arr[0]+arr[1]);
-	dp[2] = big(arr[1] + arr[2], arr[0] + arr[2]);
-	for(int i=3; i<n; i++)
-		dp[i]=big(arr[i] + dp[i-2], arr[i] + arr[i-1] + dp[i-3]);
-	
-	printf("%d\n",dp[n-1]);
-	return 0;
+        for(int i=0; i<N; i++)
+                scanf("%d",&arr[i]);
+        dp[0] = arr[0];
+        dp[1] = max(arr[0],arr[0]+arr[1]);
+        dp[2] = max(arr[1]+arr[2], arr[0]+arr[2]);
+        for(int i=3; i<N; i++)
+                dp[i] = max(arr[i] + dp[i-2], arr[i] + arr[i-1] + dp[i-3]);
+        printf("%d\n",dp[N-1]);
+        return 0;
 }
+
