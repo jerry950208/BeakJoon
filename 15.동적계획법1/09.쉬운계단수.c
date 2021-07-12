9e19f332da064e6d965f3e43ec332e49309735d5
jerry950208 <jerry950208@naver.com>
2021-07-12 20:09:52 +0900

    BaekJoon

--- 15.동적계획법1/09.쉬운계단수.c
+++ 15.동적계획법1/09.쉬운계단수.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#define mod 1000000000
 
+int main(){
+	int N,sum = 0;
+	int dp[101][10] = {0};
+	scanf("%d",&N);
+
+	for(int i=0; i<10; i++)
+		dp[1][i] = 1;
+	for(int i=2; i<=N; i++){
+		for(int j=0; j<10; j++){
+			if(j == 0)
+				dp[i][0] = dp[i-1][1] % mod;
+			else if(j == 9)
+				dp[i][9] = dp[i-1][8] % mod;
+			else
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+		}
+	}
+	for(int i=1; i<10; i++)
+		sum = (sum + dp[N][i]) % mod;
+	printf("%d\n", sum%mod);
+	
+	return 0;
+}
