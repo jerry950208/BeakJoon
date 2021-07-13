523ff8eb31d41832a020ac72b1a966ed9e7fbd98
jerry950208 <jerry950208@naver.com>
2021-07-13 14:47:06 +0900

    BaekJoon

--- 12.정렬/04.통계학.c
+++ 12.정렬/04.통계학.c
@@ -1,4 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int static compare(const void* first, const void* second){
+	if(*(int *)first > *(int *)second)
+		return 1;
+	else if(*(int *)first < *(int *)second)
+		return -1;
+	else
+		return 0;
+}
+void func_avg(int arr[], int N){
+	float k = 0.0;
+	for(int i=0; i<N; i++)
+		k += arr[i];
+	printf("%.0f\n",k/N);
+}
+void func_mid(int arr[], int N){
+	printf("%d\n",arr[N/2]);
+}
+void func_mod(int arr[], int N){
+	int b[8001] = { 0 };
+	int max = 0, cnt = 0, idx;
+	for(int i=0; i<N; i++){
+		idx = arr[i] + 4000;
+		b[idx] += 1;
+
+		if(b[idx] > max)
+			max = b[idx];
+	}
+
+	for(int i=0,idx=0; i<8001; i++){
+		if(b[i] == 0)
+			continue;
+		if(b[i] == max){
+			if(cnt == 0){
+				idx = i;
+				cnt += 1;
+			}
+			else if(cnt == 1){
+				idx = i;
+				break;
+			}
+		}
+	}
+	printf("%d\n",idx - 4000);
+}
+void func_len(int arr[], int N){
+	int max = arr[N-1];
+	int min = arr[0];
+
+	printf("%d\n",max - min);
+}
+
+int main(){
+	int N,arr[500001] = {};
+	scanf("%d",&N);
+	
+	for(int i=0; i<N; i++)
+		scanf("%d",&arr[i]);
+	qsort(arr, N, sizeof(int), compare);
+
+	if(N == 1)
+		printf("%d\n%d\n%d\n0\n",arr[0],arr[0],arr[0]);
+	else{
+		func_avg(arr,N);
+		func_mid(arr,N);
+		func_mod(arr,N);
+		func_len(arr,N);
+	}
+	return 0;
+}
+
+
+
+/*#include <stdio.h>
 void merge(int arr[], int low, int mid, int high){
         int b[500001];
         int i = low;
@@ -85,4 +160,4 @@ int main(){
                 func_len(arr,N);//범위
         }
         return 0;
-}
+}*/
