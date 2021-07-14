d1acc29848c84bbf8de167741c03a11108ed1789
jerry950208 <jerry950208@naver.com>
2021-07-14 16:19:34 +0900

    BaekJoon

--- BaekJoon_C/12.정렬/04.통계학.c
+++ BaekJoon_C/12.정렬/04.통계학.c
@@ -13,40 +13,49 @@ void func_avg(int arr[], int N){
 	float k = 0.0;
 	for(int i=0; i<N; i++)
 		k += arr[i];
-	printf("%.0f\n",k/N);
+	printf("\navg = %.0f\n",k/N);
 }
 void func_mid(int arr[], int N){
-	printf("%d\n",arr[N/2]);
+	printf("mid = %d\n",arr[N/2]);
 }
 void func_mod(int arr[], int N){
-	int b[8001] = { 0 };
 	int a[8001] = { 0 };
+	int b[8001] = { 0 };
 	int cnt = 0, cnt2 = 0;
 	
 	for(int i=0; i<N; i++){
 		for(int j=0; j<=8000; j++){
 			if(arr[i] == j-4000){
 				b[j] += 1;
-				if(cnt < b[j])
+				if(cnt <= b[j])
 					cnt = b[j];
 				break;
 			}
 		}
 	}
-
+	
 	for(int i=0; i<=8000; i++){
 		if(b[i] == cnt){
 			a[cnt2] = i - 4000;
-			cnt2++;
+			
+			if(cnt != 2){
+				printf("mod2 = %d\n",arr[1]);
+				break;
+			}
+
+			else if(cnt >=2 && cnt2 == 1){
+				printf("mod1 = %d\n",a[cnt2]);
+				break;
+			}
+			cnt2 += 1;
 		}
 	}
-	printf("%d\n",a[1]);
 }
 void func_len(int arr[], int N){
 	int max = arr[N-1];
 	int min = arr[0];
 
-	printf("%d\n",max - min);
+	printf("len = %d\n",max - min);
 }
 
 int main(){
@@ -56,6 +65,8 @@ int main(){
 	for(int i=0; i<N; i++)
 		scanf("%d",&arr[i]);
 	qsort(arr, N, sizeof(int), compare);
+	
+	printf("\n\n");
 
 	if(N == 1)
 		printf("%d\n%d\n%d\n0\n",arr[0],arr[0],arr[0]);
