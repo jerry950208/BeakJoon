0303c3419dc38066c4d09b7f9cb3857dfaf84166
jerry950208 <jerry950208@naver.com>
2021-07-14 14:59:41 +0900

    BaekJoon

--- BaekJoon_C/12.정렬/04.통계학.c
+++ BaekJoon_C/12.정렬/04.통계학.c
@@ -20,30 +20,27 @@ void func_mid(int arr[], int N){
 }
 void func_mod(int arr[], int N){
 	int b[8001] = { 0 };
-	int max = 0, cnt = 0, idx;
+	int a[8001] = { 0 };
+	int cnt = 0, cnt2 = 0;
+	
 	for(int i=0; i<N; i++){
-		idx = arr[i] + 4000;
-		b[idx] += 1;
-
-		if(b[idx] > max)
-			max = b[idx];
-	}
-
-	for(int i=0,idx=0; i<8001; i++){
-		if(b[i] == 0)
-			continue;
-		if(b[i] == max){
-			if(cnt == 0){
-				idx = i;
-				cnt += 1;
-			}
-			else if(cnt == 1){
-				idx = i;
+		for(int j=0; j<=8000; j++){
+			if(arr[i] == j-4000){
+				b[j] += 1;
+				if(cnt < b[j])
+					cnt = b[j];
 				break;
 			}
 		}
 	}
-	printf("%d\n",idx - 4000);
+
+	for(int i=0; i<=8000; i++){
+		if(b[i] == cnt){
+			a[cnt2] = i - 4000;
+			cnt2++;
+		}
+	}
+	printf("%d\n",a[1]);
 }
 void func_len(int arr[], int N){
 	int max = arr[N-1];
@@ -70,94 +67,3 @@ int main(){
 	}
 	return 0;
 }
-
-
-
-/*#include <stdio.h>
-void merge(int arr[], int low, int mid, int high){
-        int b[500001];
-        int i = low;
-        int j = mid + 1;
-        int k = 0;
-
-        while(i <= mid && j <= high){
-                if(arr[i] <= arr[j])
-                        b[k++] = arr[i++];
-                else
-                        b[k++] = arr[j++];
-        }
-        while(i <= mid)
-                b[k++] = arr[i++];
-
-        while(j <= high)
-                b[k++] = arr[j++];
-
-        k--;
-
-        while(k >= 0){
-                arr[low+k] = b[k];
-                k--;
-        }
-}
-void margesort(int arr[], int low, int high){
-        int mid;
-        if(low < high){
-                mid = (low + high)/2;
-                margesort(arr, low, mid);
-                margesort(arr, mid+1, high);
-                merge(arr, low, mid, high);
-        }
-}
-
-void func_avg(int arr[], int N){//산술평균 함수
-        float avg = 0.0;
-        for(int i=0; i<N; i++){
-                avg += arr[i];
-        }
-        printf("%.0f\n",(avg/N));
-}
-
-void func_mid(int arr[], int N){//중앙값 함수
-	printf("%d\n",arr[N/2]);
-}
-void func_min(int arr[], int N){//최빈값 함수
-	//최빈값하고 최빈값중 두번째로 작은거
-	int min[8001] = {0};
-	int cnt=0,temp=0;
-	for(int i=0; i<N; i++){
-		if(arr[i] != temp && b[i] == 0)
-	}
-}
-void func_len(int arr[], int N){//범위 함수
-	if(arr[0] >= 0)
-		printf("%d\n",arr[0] + arr[N-1]);
-	else if(arr[0] < 0){
-		if(arr[N-1] >= 0)
-			printf("%d\n",arr[N-1] - arr[0]);
-		else
-			printf("%d\n",arr[N-1] - arr[0]);
-	}
-}
-
-int main(){
-        int N;
-
-        scanf("%d",&N);
-        int arr[N+1];
-
-        for(int i=0; i<N; i++)
-                scanf("%d",&arr[i]);
-
-        margesort(arr,0,N-1);
-        printf("\n\n\n");
-        if(N == 1){
-                printf("%d\n%d\n%d\n0\n",arr[0],arr[0],arr[0]);
-        }
-        else{
-                func_avg(arr,N);//산술평균
-                func_mid(arr,N);//중앙값
-                func_min(arr,N);//최빈값
-                func_len(arr,N);//범위
-        }
-        return 0;
-}*/
--- BaekJoon_C/12.정렬/06.좌표정리하기.c
+++ BaekJoon_C/12.정렬/06.좌표정리하기.c
@@ -1,1 +1,38 @@
+//코딩 다시짜기
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct{
+	int x,y;
+}	point;
+
+point sort[100001];
+
+int compare(const void *a, const void *b);
+
+int main(){
+	
+	int N;
+	scanf("%d",&N);
+	
+	point* arr = (point*)malloc(sizeof(point)*N);
+	for(int i=0; i<N; i++)
+		scanf("%d %d",&arr[i].x, &arr[i].y);
+	qsort(arr.x,N, sizeof(int), compare );
+	for(int i=0; i<N; i++)
+		printf("%d %d \n",arr[i].x, arr[i].y);
+
+	return 0;
+}
+
+int compare(const void *a, const void *b){
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
+
+	if(num1 < num2)
+		return -1;
+	else if(num1 > num2)
+		return 1;
+	else 
+		return 0;
+}
