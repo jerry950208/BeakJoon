0e8c0ee10fecd79365741abd0b66d63744a44bea
jerry950208 <jerry950208@naver.com>
2021-07-15 15:01:18 +0900

    BaekJoon

---
+++ BaekJoon_C++/03.for문/07.A+B-7.cpp
@@ -0,0 +1,22 @@
+/*
+	A+B -7 
+	두정수를 입력받은다음 합을 출력해라
+	첫째줄에 테스트 케이스 개수 T
+	각 테스트 케이스마다 Case #x: 합 " 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int T,A,B;
+	cin >> T;
+
+	for(int i=1; i<=T; i++){
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A + B << endl;
+	}
+	return 0;
+}
