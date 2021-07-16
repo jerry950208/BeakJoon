996134dce215dbc8ec6a1a9a6c88a99dae48792d
jerry950208 <jerry950208@naver.com>
2021-07-16 21:48:07 +0900

    BaekJoon

---
+++ BaekJoon_Python/03.for문/09.별찍기-1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+N = int(input())
+
+for i in range(1,N+1):
+	print("*" * i)
---
+++ BaekJoon_Python/03.for문/10.별찍기-2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+N = int(input())
+for i in range(1,N+1):
+	print(" "*(N-i), "*"*i)
