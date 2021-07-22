9516f85bfbe4fb7ad6f273c6ee9f7c4bd7a55e0b
jerry950208 <jerry950208@naver.com>
2021-07-22 16:21:56 +0900

    BaekJoon

---
+++ BaekJoon_C++/08.기본수학/01.손익분기점.cpp
@@ -0,0 +1,24 @@
+/*
+	손익분기점
+	A = 고정비용
+	B = 가변비용
+	C = 가격
+	총수입(판매비용) > 고정비용 + 가변비용
+	손익분기점 구하시오
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int A,B,C,result;
+	cin >> A >> B >> C;
+
+	result = A / (C - B) + 1;
+	
+	if(C <= B)
+	cout << result << endl;
+	else
+	cout << "-1" << endl;
+}
