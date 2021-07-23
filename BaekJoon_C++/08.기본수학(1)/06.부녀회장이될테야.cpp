9dd132769c176bfcc66471f9ec2af80fccfaf332
jerry950208 <jerry950208@naver.com>
2021-07-23 17:53:00 +0900

    BaekJoon

---
+++ BaekJoon_C++/08.기본수학(1)/06.부녀회장이될테야.cpp
@@ -0,0 +1,12 @@
+/*
+	부녀회장이 될테야
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int T,k,n;
+}
--- BaekJoon_C/11.브루트_포스/04.체스판다시칠하기.c
+++ BaekJoon_C/11.브루트_포스/04.체스판다시칠하기.c
@@ -1,69 +1,45 @@
 #include <stdio.h>
 
-char chess[50][51];
-int cnt1,cnt2,cnt3,cnt4,min = 65;
-
-int func_min(int a,int b){
-	return a < b ? a :b;
-}
-
-void func_chess(int a, int b){
-	
-	cnt1=0;
-	cnt2=0;
-	cnt3=0;
-	cnt4=0;
-
-	for(int i=a; i<a+8; i++){
-		for(int j=b; j<=b+8; j++){
-			if(j != b+8){
-				chess[a][b]='W';
-				if((i+j)%2 == 0 && chess[i][j] == 'W')
-					cnt1++;
-				else if((i+j)%2 == 1 && chess[i][j] == 'B')
-					cnt1++;
-				if((i+j)%2 == 0 && chess[i][j] == 'B')
-					cnt2++;
-				else if((i+j)%2 == 1 && chess[i][j] == 'W')
-					cnt2++;
-				
-				chess[a][b]='B';
-				if((i+j)%2 == 0 && chess[i][j] == 'W')
-					cnt3++;
-				else if((i+j)%2 == 1 && chess[i][j] == 'B')
-					cnt3++;
-				if((i+j)%2 == 0 && chess[i][j] == 'B')
-					cnt4++;
-				else if((i+j)%2 == 1 && chess[i][j] == 'W')
-					cnt4++;
-			}
-		}
-	}
-	printf("%d %d %d %d %d %d\n",a,b,cnt1,cnt2,cnt3,cnt4);
-	if(min > func_min(cnt1,cnt2) || min > func_min(cnt3,cnt4))
-		min = (func_min(cnt1,cnt2) <= func_min(cnt3,cnt4) ? func_min(cnt1,cnt2) : func_min(cnt3,cnt4));
-}
-
+#define minFinder(a , b) a < b ? a : b
 
 int main(){
-	int N,M;
-	int N_div,M_div;
+	
+	char board[50][50];
+
+	int N, M;
+	int min = 80;
+	int cntB = 0, cntW = 0;
+
 	scanf("%d %d",&N, &M);
 
-	for(int i=0; i<N; i++){
-		for(int j=0; j<=M; j++){
-			scanf("%c",&chess[i][j]);
+	for(int i = 0; i < N; i++)
+		scanf("%s", &board[i]);
+
+	for(int i = 0; i < N - 7; i++){
+		for(int j = 0; j < M - 7; j++){
+			cntB = 0;
+			cntW = 0;
+			for(int a = i; a < i + 8; a++){
+				for(int b = j; b < j + 8; b++){
+					if((a + b) % 2 == 0){
+						if(board[a][b] == 'B')
+							cntW++;
+						else
+							cntB++;
+					}
+					else{
+						if(board[a][b] == 'B')
+							cntB++;
+						else
+							cntW++;
+					}
+				}
+			}
+			min = minFinder(min, cntB);
+			min = minFinder(min, cntW);
 		}
 	}
+	printf("%d\n", min);
 
-	N_div = N-7;
-	M_div = M-7;
-
-	for(int i=0; i<=N_div; i++){
-		for(int j=0; j<=M_div; j++){
-			func_chess(i,j);
-		}
-	}
-	printf("%d\n",min);
 	return 0;
 }
