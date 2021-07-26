62f569d2a84e654e92d7cb78aa993dd5ee896c1f
jerry950208 <jerry950208@naver.com>
2021-07-26 16:28:28 +0900

    BaekJoon

--- BaekJoon_C++/08.기본수학(1)/06.부녀회장이될테야.cpp
+++ BaekJoon_C++/08.기본수학(1)/06.부녀회장이될테야.cpp
@@ -2,27 +2,38 @@
 	부녀회장이 될테야
 
 */
-int apt[14][15];
-
-void func_apt(){
-	
-	for(int i=0; i<15; i++)
-}
-
 #include <iostream>
 
 using namespace std;
+
+int apt[15][16];
+
+void func_apt(){
+	
+	for(int i=0; i<15; i++){
+		apt[i][1] = 1;
+		apt[0][i] = i;
+	}
+
+	for(int i=0; i<15; i++){
+		for(int j=1; j<16; j++){
+			if(apt[i][j] == 0){
+				apt[i][j] = apt[i-1][j] + apt[i][j-1];
+			}
+		}
+	}
+}
 
 int main(){
 	
 	int T,k,n;
 	
 	func_apt();
-
+	
 	cin >> T;
 
 	for(int i=0; i<T; i++){
-		cin >> k >> n endl;
+		cin >> k >> n;
 		cout << apt[k][n] << endl;
 	}
 }
