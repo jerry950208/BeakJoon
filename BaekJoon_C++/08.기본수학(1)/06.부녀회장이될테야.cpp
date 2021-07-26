4ae7a232dee2be178b51e23e79e7700f94bd41c1
jerry950208 <jerry950208@naver.com>
2021-07-26 15:27:34 +0900

    BaekJoon

--- BaekJoon_C++/08.기본수학(1)/06.부녀회장이될테야.cpp
+++ BaekJoon_C++/08.기본수학(1)/06.부녀회장이될테야.cpp
@@ -2,6 +2,13 @@
 	부녀회장이 될테야
 
 */
+int apt[14][15];
+
+void func_apt(){
+	
+	for(int i=0; i<15; i++)
+}
+
 #include <iostream>
 
 using namespace std;
@@ -9,4 +16,13 @@ using namespace std;
 int main(){
 	
 	int T,k,n;
+	
+	func_apt();
+
+	cin >> T;
+
+	for(int i=0; i<T; i++){
+		cin >> k >> n endl;
+		cout << apt[k][n] << endl;
+	}
 }
--- BaekJoon_C/12.정렬/04.통계학.c
+++ BaekJoon_C/12.정렬/04.통계학.c
@@ -1,80 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int static compare(const void* first, const void* second){
-	if(*(int *)first > *(int *)second)
-		return 1;
-	else if(*(int *)first < *(int *)second)
-		return -1;
-	else
-		return 0;
+int compare(const void *a, const void *b){
+        if(*(int*)a > *(int*)b)
+                return 1;
+        else if(*(int*)a < *(int*)b)
+                return -1;
+        else
+                return 0;
 }
-void func_avg(int arr[], int N){
-	float k = 0.0;
-	for(int i=0; i<N; i++)
-		k += arr[i];
-	printf("\navg = %.0f\n",k/N);
+
+void func_avg(int arr[], int T){
+
+        double avg = 0.0;
+
+        for(int i=0; i<T; i++)
+                avg += arr[i];
+
+        printf("%.0f\n",avg / T);
 }
-void func_mid(int arr[], int N){
-	printf("mid = %d\n",arr[N/2]);
+
+void func_mid(int arr[], int T){
+
+        printf("%d\n",arr[T/2]);
+
 }
-void func_mod(int arr[], int N){
-	int a[8001] = { 0 };
-	int b[8001] = { 0 };
-	int cnt = 0, cnt2 = 0;
-	
-	for(int i=0; i<N; i++){
-		for(int j=0; j<=8000; j++){
-			if(arr[i] == j-4000){
-				b[j] += 1;
-				if(cnt <= b[j])
-					cnt = b[j];
-				break;
-			}
-		}
+
+void func_mod(int arr[], int T){
+
+        int check[8001] = { 0 };
+        int max = 0, cnt = 0, tmp;
+        for(int i=0; i<T; i++){
+
+                check[arr[i] + 4000] += 1;
+                if(check[arr[i] + 4000] > max){
+                        max = check[arr[i] + 4000];
+                }
+        }
+
+        for(int i=0; i<=8001; i++){
+                if(check[i] == max){
+                        cnt++;
+                        tmp = i - 4000;
+                        if(cnt == 2){
+                                printf("%d\n",i - 4000);
+                        }
+                }
+        }
+        if(cnt == 1)
+                printf("%d\n",tmp);
+}
+
+void func_len(int arr[], int T){
+		
+	if(arr[0] < 0){
+		if(arr[T-1] >= 0)
+			printf("%d\n",arr[T-1] - arr[0]);
+		else
+			printf("%d\n",arr[T-1] - arr[0]);
 	}
-	
-	for(int i=0; i<=8000; i++){
-		if(b[i] == cnt){
-			a[cnt2] = i - 4000;
 			
-			if(cnt != 2){
-				printf("mod2 = %d\n",arr[1]);
-				break;
-			}
-
-			else if(cnt >=2 && cnt2 == 1){
-				printf("mod1 = %d\n",a[cnt2]);
-				break;
-			}
-			cnt2 += 1;
-		}
-	}
+	else
+		printf("%d\n",arr[T-1] - arr[0]);
 }
-void func_len(int arr[], int N){
-	int max = arr[N-1];
-	int min = arr[0];
-
-	printf("len = %d\n",max - min);
-}
+	
 
 int main(){
-	int N,arr[500001] = {};
-	scanf("%d",&N);
 	
-	for(int i=0; i<N; i++)
-		scanf("%d",&arr[i]);
-	qsort(arr, N, sizeof(int), compare);
+	/*
+		산술평균 : N개의 수들의 합을 N으로 나눈 값
+		중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+		최빈값 : N개의 수들 중 가장 많이 나타나는 값
+		범위 : N개의 수들 중 최댓값과 최솟값의 차이
+	*/
 	
-	printf("\n\n");
+	int T,arr[500000];
 
-	if(N == 1)
+	scanf("%d",&T);
+
+	for(int i=0; i<T; i++)
+		scanf("%d",&arr[i]);
+	
+	qsort(arr, T, sizeof(int), compare);
+
+	if(T == 1)
 		printf("%d\n%d\n%d\n0\n",arr[0],arr[0],arr[0]);
+	
 	else{
-		func_avg(arr,N);
-		func_mid(arr,N);
-		func_mod(arr,N);
-		func_len(arr,N);
+	func_avg(arr,T);
+	func_mid(arr,T);
+	func_mod(arr,T);
+	func_len(arr,T);
 	}
+
 	return 0;
 }
