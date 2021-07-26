ff9d653ef24b5c0a9f2eec8c91cd355c2c42e71e
jerry950208 <jerry950208@naver.com>
2021-07-26 23:40:55 +0900

    BaekJoon

---
+++ BaekJoon_C/14.빽트래킹/02.N과M_2.c
@@ -0,0 +1,45 @@
+/*
+	N과 M(2)
+	자연수 N과 M이 주어졌을때 아래조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램
+	
+	* 1부터 N까지 자연수 중에서 중복없이 M개를 고른 수열
+	* 고른 수열은 오름차순이어야 한다
+*/
+#include <stdio.h>
+
+int M,N;
+int result[9],check[9];
+
+void func_sqc(int k){
+	if(k == M){
+		for(int i=0; i<M; i++)
+			printf("%d ",result[i]);
+		printf("\n");
+	}
+	else{
+		for(int i=1; i<=N; i++){
+			if(k == 0){
+				result[k] = i;
+				check[i] = 1;
+				func_sqc(k+1);
+				check[i] = 0;
+			}
+
+			else if(result[k - 1] < i){
+				result[k] = i;
+				check[i] = 1;
+				func_sqc(k+1);
+				check[i] = 0;
+			}
+
+		}
+	}
+}
+
+int main(){
+	
+	scanf("%d %d",&M, &N);
+	func_sqc(0);
+
+	return 0;
+}
