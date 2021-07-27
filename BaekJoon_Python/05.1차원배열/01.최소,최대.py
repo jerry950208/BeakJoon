bb9bb8582ab9007503e02dc804a6046c71ca43ac
jerry950208 <jerry950208@naver.com>
2021-07-27 18:41:52 +0900

    BaekJoon

--- BaekJoon_Python/05.1차원배열/01.최소,최대.py
+++ BaekJoon_Python/05.1차원배열/01.최소,최대.py
@@ -1,1 +1,21 @@
+#! usr/bin/env python3
 
+
+cnt = int(input())
+numbers = list(map(int, input().split()))
+max = numbers[0]
+min = numbers[0]
+
+for i in numbers[1:]:
+        if i > max:
+                max = i
+        elif i< min:
+                min = i
+
+print(min,max)
+
+"""
+cnt = int(input())
+num = list(map(int, input().split()))
+print(min(num), max(num))
+"""
