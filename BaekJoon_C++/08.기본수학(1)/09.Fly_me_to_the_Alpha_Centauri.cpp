d947387b3dd50b6048823dd2cf08ca3fab5abb95
jerry950208 <jerry950208@naver.com>
2021-07-27 13:31:28 +0900

    BaekJoon

--- BaekJoon_C++/08.기본수학(1)/09.Fly_me_to_the_Alpha_Centauri.cpp
+++ BaekJoon_C++/08.기본수학(1)/09.Fly_me_to_the_Alpha_Centauri.cpp
@@ -1,5 +1,7 @@
 /*
-
+	Fly me to the Alpha Centauri
+	이전 작동시기에 k광년을 이동하였을때는 k-1 or k+1 광년만을 다시 이동할수 있다
+	x지점에서 y지점을 이동할때 필요한 공간이동 장치 횟수의 최솟값을 구하시오
 */
 #include <iostream>
 
@@ -7,5 +9,25 @@ using namespace std;
 
 int main(){
 	
+	int T,x,y;
+	cin >> T;
+
+	for(int i=0; i<T; i++){
+		int len = y - x;
+
+		if(len == 1)
+			cout << "1" << endl;
+		else if(len == 2)
+			cout << "2" << endl;
+		else{
+			int cnt = 3;
+			while(1){
+				if(cnt > T)
+					cnt++;
+				else
+					
+			}
+		}
+	}
 
 }
--- BaekJoon_Python/04.while문/03.더하기사이클.py
+++ BaekJoon_Python/04.while문/03.더하기사이클.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
 
-N = int(input())
-temp = N
+n = int(input())
+num = n
 cnt = 0
 
-while 1:
-	cnt = cnt + 1
-	a = temp % 10
-	b = temp / 10
-	c = (temp % 10) * 10
-	result = ((a + b) % 10) + c
-	if result == N:
-		print(cnt) 
-		break
-	else:
-		continue
+while True:
+	a = num / 10
+	b = num % 10
+	c = (a + b) % 10
+	num = (b * 10) + c
 
-"""
-N = int(input())
-temp = N
-cnt = 0
-while 1:
 	cnt = cnt + 1
-	temp = (temp % 10) + (temp / 10) + (temp % 10) * 10
-	if temp == N:
-		print(cnt)
+	if num == n:
 		break
-"""
+
+print(cnt)
