42c00d1e96dafdfdba27cb89d03556980ba1c9eb
jerry950208 <jerry950208@naver.com>
2021-08-01 12:01:17 +0900

    BaekJoon

--- BaekJoon_C/11.브루트_포스/02.분배합.c
+++ BaekJoon_C/11.브루트_포스/02.분배합.c
@@ -1,3 +1,54 @@
+/*
+	분해합
+	자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다. 
+	어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다
+	
+
+	자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+	
+	첫째줄에  답을 출력하고 생성자가 없을경우에는 0을 출력한다
+*/
+#include <stdio.h>
+
+int func_decp(int num);
+
+int main(void){
+	
+	int N;
+	scanf("%d",&N);
+
+	for(int i=1; i<=N; i++){
+		
+		int result = func_decp(i) + i;
+		if(result == N){
+			printf("%d\n",i);
+			break;
+		}
+		else if(i == N){
+			printf("0\n");
+			break;
+		}
+	}
+	return 0;
+}
+
+int func_decp(int num){
+	
+	int sum = 0;
+	while(1){
+		
+		if(num == 0)
+			break;
+		
+		else{
+			sum += num%10;
+			num /= 10;
+		}
+	}
+
+	return sum;
+}
+/*
 #include <stdio.h>
 #define MAX 1000000
 int arr[MAX];
@@ -75,4 +126,4 @@ int main(){
 	}
 	printf("%d",arr[cnt]);
 	return 0;
-}
+}*/
