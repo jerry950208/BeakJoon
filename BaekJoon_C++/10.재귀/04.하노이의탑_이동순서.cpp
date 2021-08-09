29445d475a9b22b8806337e0c2df5557f3051dc3
jerry950208 <jerry950208@naver.com>
2021-08-09 19:12:16 +0900

    BaekJoon

---
+++ BaekJoon_C++/10.재귀/04.하노이의탑_이동순서.cpp
@@ -0,0 +1,34 @@
+/*
+	하노이 탑 이동순서
+	
+	1. 한 번에 한 개의 원판만을 다른 탑으로 옯길 수 있다
+	2. 쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다
+	이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성
+	단, 이동 횟수는 최소가 되어야 한다
+*/
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int N, int start, int mid, int end);
+
+int main(void){
+	
+	int K;
+	cin >> K;
+
+	cout << (1<<K) -1 << "\n";
+	hanoi(K, 1, 3, 2);
+}
+
+void hanoi(int N, int start, int mid, int end){
+	
+	if(N == 1)
+		cout << start << " " << mid << "\n";
+	
+	else{
+		hanoi(N-1, start, end, mid);
+		cout << start << " " << mid << "\n";
+		hanoi(N-1, end, mid, start);
+	}
+}
---
+++ BaekJoon_C++/17.정수론및조합론/08.이항계수2.cpp
@@ -0,0 +1,29 @@
+/*
+	이항계수2
+	
+	자연수 N과 정수 K가 주어졌을때 nCk를 10007로 나눈 나머지를 구하는 프로그램 작성
+*/
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	
+	int N, K, result;
+	cin >> N >> K;
+	
+	int Dp[1001][1001] = { 0 };
+
+	for(int i=1; i<=N; i++){
+		for(int j=0; j<= N; j++){
+			if(i == j || j == 0){
+				Dp[i][j] = 1;
+				continue;
+			}
+			Dp[i][j] = (Dp[i-1][j] + Dp[i-1][j-1]) % 10007;
+		}
+	}
+	cout << Dp[N][K] << "\n";
+}
+*/
