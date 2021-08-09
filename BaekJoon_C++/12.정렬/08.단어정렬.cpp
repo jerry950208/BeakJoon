b464fbea055c745f1f7d107bd12e33c3b954ac7c
jerry950208 <jerry950208@naver.com>
2021-08-09 23:07:20 +0900

    BaekJoon

--- BaekJoon_C++/12.정렬/08.단어정렬.cpp
+++ BaekJoon_C++/12.정렬/08.단어정렬.cpp
@@ -4,32 +4,111 @@
 	1. 길이가 짧은 것부터
 	2. 길이가 같으면 사전순으로
 */
-/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-bool compareWith(string a, string b){
-  if(a.length() == b.length())
-    return a<b;
-  return a.length()<b.length();
+bool compare(const string a, const string b);
+
+int main(void){
+	
+	vector<string> v;
+	string word, tmp;
+	int N;
+	cin >> N;
+
+	for(int i=0; i<N; i++){
+		cin >> word;
+		v.push_back(word);
+	}
+	
+	sort(v.begin(), v.end(), compare);
+
+	for(int i=0; i<N; i++){
+		if(tmp == v[i])
+			continue;
+		cout << v[i] << "\n";
+		tmp = v[i];
+	}
 }
 
-int main(){
-  int N;
-  vector<string> vec;  
-  cin>>N;
-  for(int i=0; i<N; i++){
-    string str;
-    cin>>str;
-    if(find(vec.begin(), vec.end(), str) == vec.end())
-      vec.push_back(str);
-  }
-  sort(vec.begin(), vec.end(), compareWith);
+bool compare(const string a, const string b){
+	
+	if(a.size() == b.size())
+		return a < b;
+	
+	else
+		return a.size() < b.size();
+}
+/*
+#include <iostream>
+#include <string>
 
-  for(int i=0; i<vec.size(); i++){
-    cout<<vec[i]<<'\n';
-  }
-}*/
+using namespace std;
+
+int compare(const void*, const void*);
+
+struct dictionary{
+	
+	int len;
+	string word;
+};
+
+dictionary words[20001];
+
+int main(void){
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, cnt = 0;
+	cin >> N;
+	
+	// 단어를 입력받고, 해당 단어의 길이를 바로 구조체에 넣는다
+	for(int i=0; i<N; i++){	
+		cin >> words[i].word;
+		words[i].len = words[i].word.length();
+	}	
+	
+	for(int j=0; j<N; j++){
+		for(int u=j+1; u<N; u++){
+			// 중복되는 단어는 지워준다
+			if(words[j].word == words[u].word){
+				words[u].word.clear();
+			}
+
+			// 단어의 길이가 같은경우 사전순으로 정렬한다
+			else if(words[j].len == words[u].len && words[j].word > words[u].word){
+				string tmp;
+				tmp = words[j].word;
+				words[j].word = words[u].word;
+				words[u].word = tmp;
+			}
+		}
+	}
+	// 단어의 길이별로 정렬한다
+	qsort(words, N, sizeof(dictionary), compare);	
+	
+	// 출력
+	for(int i=0; i<N; i++){
+		if(words[i].word.length() > 0){
+			cout << words[i].word << " " << "\n";
+		}
+	}
+}
+
+int compare(const void* a, const void* b){
+	
+	if(*(int*)a > *(int*)b)
+		return 1;
+
+	else if(*(int*)a < *(int*)b)
+		return -1;
+
+	else
+		return 0;
+}
+*/
