dd86c1dc8803918e5f1f1a61881583c5302598a4
jerry950208 <jerry950208@naver.com>
2021-08-09 14:02:09 +0900

    BaekJoon

---
+++ BaekJoon_C++/17.정수론및조합론/04.최소공배수.cpp
@@ -0,0 +1,47 @@
+/*
+        최소공배수
+
+        두 자연수 A와 B가 주어졌을 때, A와 B의 최소 공배수를 구하는 프로그램을 작성
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int func_LCM(int a, int b);
+
+int main(void){
+
+        int N;
+        int min[1001], max[1001];
+        cin >> N;
+
+        for(int i=0; i<N; i++){
+                cin >> min[i] >> max[i];
+
+                cout << func_LCM(min[i], max[i]) << "\n";
+        }
+}
+
+int func_LCM(int a, int b){
+
+        int cnt = 2, lcm = 1;
+
+        while(1){
+
+                if(a <= cnt && b <= cnt){
+                        lcm *= a * b;
+                        break;
+                }
+
+                else if(a % cnt == 0 && b % cnt == 0){
+                        lcm *= cnt;
+                        a /= cnt;
+                        b /= cnt;
+                }
+
+                else
+                        cnt++;
+        }
+        return lcm;
+}
