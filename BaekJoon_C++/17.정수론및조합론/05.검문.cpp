095d723ebd9fe47c5eb54638adb63bc9e4c886e2
jerry950208 <jerry950208@naver.com>
2021-08-11 16:06:40 +0900

    BaekJoon

--- BaekJoon_C++/17.정수론및조합론/05.검문.cpp
+++ BaekJoon_C++/17.정수론및조합론/05.검문.cpp
@@ -4,58 +4,50 @@
 
 	N개의 수가 주어졌을때, 가능한 M을 모두 찾는 프로그램을 작성하시오
 */
+//* gcd(arr[n]-arr[n-1])
+//1. 나머지 정리를 통한 식 변형
+//2. 최대공약수를 어떻게 찾을 것인가
+//3. 매우 큰 수의 약수를 어떻게 구할것인가
+//4. 큰 수의 약수들은 순서대로 어떻게 출력할 것인가
+
 #include <iostream>
-
+#include <algorithm>
+ 
 using namespace std;
+ 
+int arr[101], sol[101]; 
+int func_Gcd(int a, int b);
 
-int compare(const void*, const void*);
-int gcd(int a, int b);
+int main() {
+   
+   int N, tmp,cnt = 0;
+    cin >> N;
 
-int main(void){
-	
-	int N,num[101], result[101];
-	int cnt = 0;
-	cin >> N;
-	
-	for(int i=0; i<N; i++)
-		cin >> num[i];
-
-	qsort(num, N, sizeof(int), compare);
-	
-	int tmp = num[1] - num[0];
-	for(int i=2; i<N; i++)
-		tmp = gcd(tmp, num[i] - num[i-1]);
-
-	for(int i=1; i*i <= tmp; i++){
-		if(tmp % i == 0){
-			result[cnt++] = i;
-			if(i != tmp / i)
-				result[cnt++] = tmp / i;
-		}
-	}
-
-	qsort(result,N,sizeof(int),compare);
-	
-	for(int i=0; i<cnt; i++){
-		if(result[i] != 1)
-			cout << result[i] << " ";
-	}
-	cout << "\n";
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+   
+    sort(arr, arr + N);
+ 
+    tmp = arr[1] - arr[0];
+    
+	for (int i = 2; i < N; i++)
+        tmp = func_Gcd(tmp, arr[i] - arr[i - 1]);
+ 
+    for (int i = 1; i*i <= tmp; i++)
+        if (tmp % i == 0) {
+            sol[cnt++] = i;
+            if (i != tmp / i) 
+				sol[cnt++] = tmp / i;
+        }
+ 
+    sort(sol, sol + cnt);
+    
+	for (int i = 0; i < cnt; i++) 
+        if (sol[i] != 1)
+            cout << sol[i] << " ";
+    
 }
 
-int compare(const void* a, const void* b){
-	
-	if(*(int*)a > *(int*)b)
-		return 1;
-
-	else if(*(int*)a < *(int*)b)
-		return -1;
-
-	else
-		return 0;
-}
-
-int gcd(int a, int b){
-	
-	return a % b ? gcd(b, a%b) : b;
+int func_Gcd(int a, int b){
+	return a % b ? func_Gcd(b, a%b) : b;
 }
