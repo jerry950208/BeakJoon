a17e93dc210b6ef4ff585ad44b2c0a31c0cdc929
jerry950208 <jerry950208@naver.com>
2021-08-18 16:36:41 +0900

    BaekJoon

--- BaekJoon_C++/18.스택/06.오큰수.cpp
+++ BaekJoon_C++/18.스택/06.오큰수.cpp
@@ -2,7 +2,37 @@
 	Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수중에서 가장 왼쪽에 있는 수를 의미한다(그러한 수가 없는 경우에 오큰수는 -1이다)
 	Ex) A = [3, 5, 2, 7] 인경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1
 */
+#include <iostream>
+#include <stack>
+using namespace std;
 
+int main(void){
+	
+	int N, arr[1000000], sequence[1000000];
+	stack<int> s;
+	
+	cin>> N;
+
+	for(int i=0; i<N; i++)
+		cin >> sequence[i];
+
+	for(int i=N-1; i>=0; i--){
+		
+		while(!s.empty() && sequence[i] >= s.top())
+			s.pop();
+
+		if(s.empty())
+			arr[i] = -1;
+		else
+			arr[i] = s.top();
+
+		s.push(sequence[i]);
+	}
+
+	for(int i=0; i<N; i++)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
 /*	시간초과
 #include <iostream>
 #include <stack>
