2ca1ac8a53fb61f86550586a950d02096c9e0fa9
jerry950208 <jerry950208@naver.com>
2021-08-19 20:46:37 +0900

    solve

--- BaekJoon_C++/14.백트래킹/06.스도쿠.cpp
+++ BaekJoon_C++/14.백트래킹/06.스도쿠.cpp
@@ -1,6 +1,23 @@
 /*
 	스도쿠
 */
+#include <iostream>
+using namespace std;
+
+int main(void){
+	
+	int arr[10][10], cnt = 0;
+	
+	for(int i=1; i<10; i++){
+		for(int j=1; j<10; j++){
+			cin >> arr[i][j];
+			if(arr[i][j] == 0)
+				cnt++;
+		}
+	}
+	cout << cnt << endl;
+}
+
 /*
 #include <iostream>
 
--- BaekJoon_C++/16.그리디알고리즘/02.회의실배정.cpp
+++ BaekJoon_C++/16.그리디알고리즘/02.회의실배정.cpp
@@ -1,3 +1,30 @@
 /*
 	회의실배정
 */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+	
+	int N;
+	cin >> N;
+	vector<pair<int, int> > v(N);
+
+	for(int i=0; i<N; i++)
+		cin >> v[i].second >> v[i].first;
+
+	sort(v.begin(), v.end());
+
+	int conference = 0, cnt = 0;
+	
+	for(int i=0; i<N; i++){
+		if(conference <= v[i].second){
+
+			conference = v[i].first;
+			cnt++;
+		}
+	}
+	cout << cnt << "\n";
+}
