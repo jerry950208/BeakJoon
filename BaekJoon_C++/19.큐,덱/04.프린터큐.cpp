90dbade9021d63c50b1d6bfbc9cd201bbdc0e745
jerry950208 <jerry950208@naver.com>
2021-09-03 19:30:44 +0900

    Not solve

--- BaekJoon_C++/19.큐,덱/04.프린터큐.cpp
+++ BaekJoon_C++/19.큐,덱/04.프린터큐.cpp
@@ -6,13 +6,9 @@
 	2. 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면 
 	이 문서를 인쇄하지 않고 Queue의 가장 뒤에 재배치한다 그렇지 않으면 바로 인쇄를 한다
 */
-
-
-/*
-//큐는 인덱스 확인 불가
 #include <iostream>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 int main(void){
@@ -21,36 +17,91 @@ int main(void){
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int Testcase;
-	cin >> Testcase;
+	int TestCase;
+	cin >> TestCase;
+
+	for(int C=0; C<TestCase; C++){
+		
+		int N, M, answer = 0;
+		cin >> N >> M;
+
+		int tmp;
+		if(N == 1){
+			answer = 1;
+			cin >> tmp;
+		}
+		else{
+			queue<pair<int, int> > q;
+			int input, idx, cnt = 0;
+			
+			for(int i=0 ; i<N; i++){
+				cin >> input;
+				q.push(make_pair(i, input));		//q.second 에 입력값 저장
+				if(i == M)
+					idx = input;					//idx 에 M번째 인덱스 값을 넣는다
+				
+				else
+					cnt++;
+			}
+
+			while(1){
+				if(q.front().second >= idx && q.front().first != M && cnt > 0){
+					q.pop();
+					answer++;
+					cnt--;
+				}
+
+				else if(q.front().second == idx && q.front().first == M || cnt == 0) break;
+				
+				else{
+					q.push(q.front());
+					q.pop();
+					answer++;
+					cnt--;
+				}
+			}
+		}
+
+		cout << answer << "\n";
+	}
+}
+/*
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(void){
 	
-	for(int i=0; i<Testcase; i++){
-		int N, idx;
-		cin >> N >> idx;
+	int TestCase;
+	cin >> TestCase;
+
+	for(int i=0; i<TestCase; i++){
+		//queue<pair<int, int> > q;
+		priority_queue<int> q;
+		int N, M;
+		cin >> N >> M;
 		
-		int input;
-		queue<pair<int, int> > q;
-		for(int i=0; i<N; i++){
+		if(N == 1){
+			cout << "1" << "\n";
+			break;
+		}
+		
+		int input, idx,answer = 0;
+		for(int j=0; j<N; j++){
 			cin >> input;
-			q.push(make_pair(i, input));
+			q.push(input);
+			if(j = M)
+				idx = input;
 		}
+
 		
-		int cnt = 0;
-		for(int i=0; i<N; i++){
-			if(q[i].second > q[idx].second){
-				q[i].pop();
-				cnt++;
+		for(int j=0; j<N; j++){
+			if(q.front() > idx){
+				q.pop();
+				answer++;
 			}
+			
 		}
-
-		for(int i=0; i<q.size(); i++){
-			if(q[i].second == q[idx].second){
-				cnt++;
-				if(i == idx)
-					break;
-			}
-		}
-
-		cout << cnt << "\n";
 	}
 }*/
