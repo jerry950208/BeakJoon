471bef44c6aeb8076fae3d2377a977edac43fb9c
jerry950208 <jerry950208@naver.com>
2021-09-14 22:03:31 +0900

    Study Java

---
+++ BaekJoon_Java/01.입출력과_사칙연산/01.Hello_World.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.print("Hello World!");
+		
+		//아래와 같이 출력을 해줄수도 있
+		//Syetem.out.println("Hello World!");
+		//Syetem.out.printf("Hello World!");
+		//Syetem.out.printf("%s", Hello World!");
+	}
+}
