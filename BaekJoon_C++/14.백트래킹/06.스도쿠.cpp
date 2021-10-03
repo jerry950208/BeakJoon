fe7d4557f4ba8a63b2f8af35156d53e73ff14da1
jerry950208 <jerry950208@naver.com>
2021-10-03 22:17:00 +0900

    Sudoku not solve

---
+++ BaekJoon_C++/14.백트래킹/06.스도쿠.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void){
+	
+	vector<vector<int> > sudoku_board;
+	int n;
+
+	cin >> sudoku_board;
+	
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+
+		}
+		cout << "\n";
+	}
+	return 0;
+}
