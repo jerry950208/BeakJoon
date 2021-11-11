0dbeec499e5bbee8c33413cea5cc1ba2221d22e3
jerry950208 <jerry950208@naver.com>
2021-11-11 16:50:18 +0900

    Solve Q2739

---
+++ BaekJoon_Java/03.for문/Q2739.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q2739{
+	
+	public static void main(String[] args){
+	
+		int N;
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		for(int i=1; i<=9; i++)
+			System.out.println(N + " * " + i + " = " + (N * i));
+	}
+}
