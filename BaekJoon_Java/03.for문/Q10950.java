e12bbac3fcb3fb7251055600ffb001b5f383d51f
jerry950208 <jerry950208@naver.com>
2021-11-11 16:56:50 +0900

    Solve Q10950

---
+++ BaekJoon_Java/03.for문/Q10950.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q10950{
+
+	public static void main(String[] args){
+		
+		int T;
+		Scanner sc = new Scanner(System.in);
+
+		T = sc.nextInt();
+
+		for(int i=0; i<T; i++){
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			System.out.println(A + B);
+		}
+	}
+}
