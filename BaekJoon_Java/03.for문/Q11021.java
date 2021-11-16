006845ef53aa8e1821df0977624425ceffacc2c7
jerry950208 <jerry950208@naver.com>
2021-11-16 15:13:11 +0900

    Solve Q11021

---
+++ BaekJoon_Java/03.for문/Q11021.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q11021{
+	
+	public static void main(String[] args){
+		
+		int T;
+		Scanner sc = new Scanner(System.in);
+
+		T = sc.nextInt();
+
+		for(int i=1; i<=T; i++){
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			System.out.println("Case #" + i + ": " + (A+B));
+		}
+	}
+}
