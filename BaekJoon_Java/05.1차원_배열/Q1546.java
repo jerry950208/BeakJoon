0d1349e8ea582e4c555a07edd0fb0c05b89b67dc
jerry950208 <jerry950208@naver.com>
2021-11-19 14:28:36 +0900

    Solve Q1546

--- BaekJoon_Java/05.1차원_배열/Q1546.java
+++ BaekJoon_Java/05.1차원_배열/Q1546.java
@@ -3,6 +3,35 @@ import java.util.Arrays;
 
 public class Q1546{
 
+        public static void main(String[] args){
+
+                Scanner in = new Scanner(System.in);
+                int n = in.nextInt();
+                double sum = 0;
+                int max = 0;
+                int arr[] = new int[n];
+
+                Arrays.fill(arr,0);
+
+                for(int i=0; i<n; i++){
+                        arr[i] = in.nextInt();
+                        if(arr[i] > max)
+                                max = arr[i];
+                }
+
+                for(int i=0; i<n; i++){
+                        sum += ((100.0 * arr[i])/max);
+                }
+                System.out.printf("%.5f%n",sum / n);
+        }
+}
+
+/*
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Q1546{
+
 	public static void main(String[] args){
 
 		Scanner in = new Scanner(System.in);
@@ -20,4 +49,4 @@ public class Q1546{
 
 		System.out.println(sum / arr.length);
 	}
-}
+}*/
---
+++ BaekJoon_Java/05.1차원_배열/Q8958.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Q8958{
+
+	public static void main(String[] args){
+
+		Scanner in = new Scanner(System.in);
+		String arr[] = new String[in.nextInt()];
+
+		for(int i=0; i<arr.length; i++)
+			arr[i] = in.next();
+
+		in.close();
+
+		for(int i=0; i<arr.length; i++){
+
+			int cnt = 0;
+			int sum = 0;
+			
+			for(int j=0; j<arr[i].length(); j++){
+				if()
+			}
+		}
+	}
+}
