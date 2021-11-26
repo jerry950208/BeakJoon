5c596a5e0626b40a546f95e9c72f8c8eef35909d
jerry950208 <jerry950208@naver.com>
2021-11-26 23:34:54 +0900

    Q1546

--- BaekJoon_Java/05.1차원_배열/Q1546.java
+++ BaekJoon_Java/05.1차원_배열/Q1546.java
@@ -3,50 +3,24 @@ import java.util.Arrays;
 
 public class Q1546{
 
-        public static void main(String[] args){
-
-                Scanner in = new Scanner(System.in);
-                int n = in.nextInt();
-                double sum = 0;
-                int max = 0;
-                int arr[] = new int[n];
-
-                Arrays.fill(arr,0);
-
-                for(int i=0; i<n; i++){
-                        arr[i] = in.nextInt();
-                        if(arr[i] > max)
-                                max = arr[i];
-                }
-
-                for(int i=0; i<n; i++){
-                        sum += ((100.0 * arr[i])/max);
-                }
-                System.out.printf("%.5f%n",sum / n);
-        }
-}
-
-/*
-import java.util.Scanner;
-import java.util.Arrays;
-
-public class Q1546{
-
 	public static void main(String[] args){
 
 		Scanner in = new Scanner(System.in);
-		double arr[] = new double[in.nextInt()];	//입력받은 만큼 배열 생성
-		
-		for(int i=0; i<arr.length; i++)
-			arr[i] = in.nextDouble();
-		in.close();
-
+		int subject = in.nextInt();
+		int[] score = new int[subject];
 		double sum = 0;
-		Arrays.sort(arr);	// 최대값을 구하기 위해 정렬해준다
+		int max = 0;
 
-		for(int i=0; i<arr.length; i++)
-			sum += ((arr[i] / arr[arr.length-1]) * 100);
+		for(int i=0; i<subject; i++){
+			score[i] = in.nextInt();
+			if(max < score[i])
+				max = score[i];
+		}
 
-		System.out.println(sum / arr.length);
+		for(int i=0; i<subject; i++)
+		
+			sum += ((100.0 * score[i]) / max);
+
+		System.out.println(sum / subject);
 	}
-}*/
+}
--- BaekJoon_Java/05.1차원_배열/Q8958.java
+++ BaekJoon_Java/05.1차원_배열/Q8958.java
@@ -6,21 +6,29 @@ public class Q8958{
 	public static void main(String[] args){
 
 		Scanner in = new Scanner(System.in);
-		String arr[] = new String[in.nextInt()];
+		int T = in.nextInt();
 
-		for(int i=0; i<arr.length; i++)
-			arr[i] = in.next();
-
-		in.close();
-
-		for(int i=0; i<arr.length; i++){
-
-			int cnt = 0;
-			int sum = 0;
+		for(int i=0; i<T; i++){
 			
-			for(int j=0; j<arr[i].length(); j++){
-				if()
+			String scores;
+			int cnt = 0;
+			int result = 0;
+			
+			// Scanner에서 char를 바로 입력받는것 없다
+			// 따라서 Scanner.next()를 통해 입력받은 다음, String.charAt()으로 char를 꺼내면 된
+			scores = in.next().charAt(0);			//입력받은 다음, 첫번째 char 꺼내기
+			//scores = in.next().trim().charAt(0);	//공백을 제거한 다음 첫번째 char꺼내
+			for(int j=0; j<scores.length; j++){
+				
+				if(scores[j] == 'O')
+					result += ++cnt;
+
+				else
+					cnt = 0;
 			}
+
+			System.out.println(result);
 		}
+
 	}
 }
